efcdebe823dff3bbd4f27fe96880b0bfd74ec327
savakotsur <slendygood@gmail.com>
2024-03-18 18:26:11 +0300

    Initial Commit

---
+++ MNYchecker/MNYcheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MNYcheckerApp.swift
+//  MNYchecker
+//
+//  Created by Савелий Коцур on 18.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MNYcheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
