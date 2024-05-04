cae20b5f674754cc64375b227953490213668841
savakotsur <slendygood@gmail.com>
2024-05-05 01:59:41 +0300

    Added AddSpendView

--- MNYchecker/Applicataion/Views/AddSpendView.swift
+++ MNYchecker/Applicataion/Views/AddSpendView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct AddSpendView: View {
+    
+    @State private var spendName = ""
+    @State private var spendValue = ""
+    @State private var spendDate = Date.now
+    @State private var spendCategory = ""
+    @State private var spendDescription = ""
+    
     var body: some View {
-        Text("Добавление траты")
+        VStack (alignment: .leading){
+                TextFieldDesign(textName: "Наименование", textFieldBinding: $spendName)
+                TextFieldDesign(textName: "Стоимость", textFieldBinding: $spendValue)
+                Text("Дата")
+                    .padding(.horizontal, 35)
+                DatePicker("2", selection: $spendDate)
+                    .labelsHidden()
+                    .padding(.horizontal, 30)
+                Text("Категория")
+                    .padding(.horizontal, 35)
+                TextField("", text: $spendCategory)
+                    .padding(.horizontal, 15)
+                    .padding(.vertical, 10)
+                    .background(Color("ElementsColor"))
+                    .cornerRadius(15)
+                    .padding(.horizontal, 30)
+                Text("Описание")
+                    .padding(.horizontal, 35)
+            TextField("", text: $spendDescription, axis: .vertical)
+                    .padding(.horizontal, 15)
+                    .padding(.vertical, 10)
+                    .background(Color("ElementsColor"))
+                    .cornerRadius(15)
+                    .padding(.horizontal, 30)
+                    .lineLimit(1...6)
+            Spacer()
+        }
+        .padding(.vertical, 20)
     }
 }
 
---
+++ MNYchecker/Applicataion/Views/Components/TextFieldDesign.swift
@@ -0,0 +1,32 @@
+//
+//  TextFieldDesign.swift
+//  MNYchecker
+//
+//  Created by Савелий Коцур on 05.05.2024.
+//
+
+import SwiftUI
+
+struct TextFieldDesign: View {
+    
+    @State var textName: String
+    @State var textFieldBinding: Binding<String>
+    var body: some View {
+        VStack (alignment: .leading) {
+            Text(textName)
+                .padding(.horizontal, 35)
+            TextField("", text: textFieldBinding)
+                .padding(.horizontal, 15)
+                .padding(.vertical, 10)
+                .background(Color("ElementsColor"))
+                .cornerRadius(15)
+                .padding(.horizontal, 30)
+        }
+    }
+}
+
+//struct TextFieldDesign_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TextFieldDesign(textName: "Наименование", textFieldBinding: "Хуй")
+//    }
+//}
